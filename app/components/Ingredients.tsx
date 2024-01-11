91c79f5bd130ab7d4a06637ef6fa5e640c54f37c
Chan <chan@ipodclasic128gb.home>
2024-01-11 21:58:36 +0000

    added ingredient list saving function

--- app/components/Ingredients.tsx
+++ app/components/Ingredients.tsx
@@ -3,27 +3,22 @@ import React from "react";
 import { primaryButtonStyle } from "@/util/styles";
 import type { RootState } from "../../lib/store";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  deleteIngredientFromList,
-  setIngredients,
-} from "@/lib/features/ingredientList/ingredientListSlice";
+import { deleteIngredientFromList } from "@/lib/features/ingredientList/ingredientListSlice";
 import IngredientBoard from "./IngredientBoard";
 import { useState } from "react";
 import {
   BoardStatus,
   setShowBoard,
 } from "@/lib/features/showBoard/showBoardSlice";
-interface Ingredient {
-  ingredient: string;
-  unit: string;
-  quantity: number;
-}
+
+import useIngredientList from "../hooks/useIngredientList";
 
 export default function Ingredients() {
   const dispatch = useDispatch();
-  const ingredientList = useSelector(
-    (state: RootState) => state.ingredientList.value
-  );
+  const ingredientList = useIngredientList();
+  // const ingredientList = useSelector(
+  //   (state: RootState) => state.ingredientList.value
+  // );
   const showBoard = useSelector((state: RootState) => state.showBoard.value);
 
   const [ingredientIndex, setIngredientIndex] = useState<number | null>(null);
