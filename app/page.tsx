a38e9e47043b86540df5d9c3eee161af7e482d04
Chan <chan@iPod-Classic-128GB.local>
2024-01-25 17:01:47 +0000

    adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Search from "./components/Search";
 export default function Home() {
   return (
     <div className={"flex flex-col items-center p-5 h-screen"}>
-      <AuthHeader />
+      {/* <AuthHeader /> */}
       <Search />
       <Ingredients />
     </div>
