6871d74c40f89572c3e8e7b15dcaa0aaa288476f
Chan <chan@iPod-Classic-128GB.local>
2024-03-03 19:21:32 +0000

    adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,11 @@ export default function Home() {
   return (
     <div className={"flex flex-col items-center p-5 h-screen"}>
       <h1 className={"font-extrabold"}>
-        For an improved experience, using Chrome is recommended.
+        For an improved experience, Chrome is recommended.
       </h1>
-      <h1 className={"font-extrabold"}>For educational use only</h1>
+      <h1 className={"font-extrabold"}>
+        For educational use only. Coursework - university of West London
+      </h1>
       <AuthHeader />
       <Search />
       <Ingredients />
