d927f8bb9beaccfb9d06e791da59c42aad4e0512
Chan <chan@ipodclasic128gb.home>
2024-03-12 11:30:31 +0000

    adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import Search from "./components/Search";
 export default function Home() {
   return (
     <div className={"flex flex-col items-center p-5 h-screen"}>
-      <h1 className={"font-extrabold"}>
+      {/* <h1 className={"font-extrabold"}>
         For an improved experience, Chrome is recommended.
       </h1>
       <h1 className={"font-extrabold"}>
         For educational use only. Coursework - university of West London
-      </h1>
+      </h1> */}
       <AuthHeader />
       <Search />
       <Ingredients />
